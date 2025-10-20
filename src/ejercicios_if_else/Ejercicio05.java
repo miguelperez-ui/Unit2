7f04bbf93b29c99d158c00b4b6d7672746927183
miguel.perez <miguel.perez@106-20.info2>
2025-10-20 08:58:27 +0200

    ultimo

--- src/ejercicios_if_else/Ejercicio05.java
+++ src/ejercicios_if_else/Ejercicio05.java
@@ -5,17 +5,45 @@ import java.util.Scanner;
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
-		Scanner reader= new Scanner(System.in);
-		
-		final String PIEDRA;
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Declaramos las constantes, PIEDRA, PAPEL o TIJERA
+		final String TIJERA = "TIJERA";
+		final String PIEDRA = "PIEDRA";
+		final String PAPEL = "PAPEL";
+
+		// Declaramos las variables para la jugada 1 y la jugada 2
 		String jugada1;
 		String jugada2;
-		
-		
-		
-		
-		reader.close();
 
+		// Pedimos al usuario
+		System.out.println("Introduzca el movimineto del jugador 1 (PIEDRA, PAPEL o TIJERA): ");
+		jugada1 = reader.next();
+
+		System.out.println("Introduzca el movimineto del jugador 2 (PIEDRA, PAPEL o TIJERA:) ");
+		jugada2 = reader.next();
+
+		if (!jugada1.equals(PIEDRA) && !jugada1.equals(PAPEL) && !jugada1.equals(TIJERA)) {
+			System.out.println("Jugada 1 incorrecta");
+
+		} else if (!jugada2.equals(PIEDRA) && !jugada2.equals(PAPEL) && !jugada2.equals(TIJERA)) {
+			System.out.println("Jugada 2 incorrecta");
+
+		} else if (jugada1.equals(jugada2)) { // Si coinciden empatan
+			System.out.println("Empate : " + jugada1 + " Y " + jugada2);
+
+		} else if (jugada1.equals(PIEDRA) && jugada2.equals(TIJERA) || jugada1.equals(PAPEL) && jugada2.equals(PIEDRA)
+				|| jugada1.equals(TIJERA) && jugada2.equals(PAPEL)) {
+
+			// Si gana el jugador 1
+			System.out.println("Gana el jugador 1");
+		} else {
+			// Gana el jugador 2
+			System.out.println("Gana el jugador 2");
+		}
+
+		reader.close();
 	}
 
 }
---
+++ src/ejercicios_if_else/Ejercicio06.java
@@ -0,0 +1,42 @@
+package ejercicios_if_else;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		
+				// Creamos el Scanner
+				Scanner reader = new Scanner(System.in);
+
+				// Pedimos al usuario que intriduzca un primer número
+				System.out.print("Introduce el primer número entero: ");
+				int num1 = reader.nextInt();
+
+				// Pedimos al usuario que intriduzca un segundo número
+				System.out.print("Introduce el segundo número entero: ");
+				int num2 = reader.nextInt();
+
+				// Pedimos al usuario que intriduzca un tercer número
+				System.out.print("Introduce el tercer número entero: ");
+				int num3 = reader.nextInt();
+
+				if (num1 + num2 == num3) { // Vemos si la suma de los dos primeros números suman el tercer número
+					System.out.println("La suma de " + num1 + " y " + num2 + " es igual a " + num3);
+
+				} else if (num1 + num3 == num2) { // Vemos si la suma del primer y tercer número suman el segundo número
+					System.out.println("La suma de " + num1 + " y " + num3 + " es igual a " + num2);
+
+				} else if (num2 + num3 == num1) { // Vemos si la suma del segundo y tercer número suman el primer número
+					System.out.println("La suma de " + num2 + " y " + num3 + " es igual a " + num1);
+
+				} else { // Si no se cumplen niguna de las condiciones imprimimos por pantalla el siguiente mensaje
+					System.out.println("Ninguna suma de dos números da como resultado el otro.");
+
+					reader.close();
+				}
+
+				
+	}
+
+}
